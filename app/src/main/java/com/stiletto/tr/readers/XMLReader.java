4f62c99ba4c9b7fea399f6eaa95afdf1fe373323
Jana Kosar <yana.kosar@gmail.com>
2017-06-11 17:02:51 +0300

    GSON parser

--- app/src/main/java/com/stiletto/tr/readers/XMLReader.java
+++ app/src/main/java/com/stiletto/tr/readers/XMLReader.java
@@ -1,22 +1,20 @@
 package com.stiletto.tr.readers;
 
 import android.util.Log;
-import android.util.Xml;
 
+import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 import com.stanfy.gsonxml.GsonXml;
 import com.stanfy.gsonxml.GsonXmlBuilder;
 import com.stanfy.gsonxml.XmlParserCreator;
 
 import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
+
+import fr.arnaudguyon.xmltojsonlib.XmlToJson;
 
 /**
  * Created by yana on 10.06.17.
@@ -44,75 +42,113 @@ public class XMLReader {
         String xml = TxtReader.parseAsText(file).toString();
         FictionBook model = gsonXml.fromXml(xml, FictionBook.class);
 
+
         return model.getContent();
 
     }
+
     public static class FictionBook {
-//        title - FictionBook/body
-//        epigraph - FictionBook/body
-//
-//        section title
 
         @SerializedName("body")
         Body body;
 
 
         public String getContent() {
+            return body.getText();
+        }
+    }
+
+
+    /**
+     *
+     */
+    private static class Body{
+
+        @SerializedName("title")
+        private Title title;
+//        @SerializedName("section")
+//        private Chapter[] chapters;
+
+        public String getText(){
+
             StringBuilder builder = new StringBuilder();
-            builder.append(body.title.content.text);
+
+
+            builder.append(title.getText());
             builder.append("\n\n");
 
-            for (Section section: body.section){
-                builder.append(section.title.content.text);
-                builder.append("\n");
-                builder.append(section.content.text);
+//                for (Chapter chapter: chapters){
+//                    builder.append(chapter.getText());
+//                }
+
+
+            return builder.toString();
+        }
+}
+
+
+    private static class Title{
+        @SerializedName("p")
+        P  []parts;
+
+        public String getText(){
+
+            StringBuilder builder = new StringBuilder();
+
+            for (P part:parts){
+                builder.append(part.content);
+            }
+
+
+            return builder.toString();
+        }
+
+    }
+
+    private static class P{
+
+        @SerializedName("$")
+        String content;
+    }
+    private static class Chapter {
+
+        @SerializedName("title")
+        private Title title;
+        @SerializedName("section")
+        private List<Page> pages;
+
+        public String getText(){
+
+            StringBuilder builder = new StringBuilder();
+            builder.append(title.getText());
+            builder.append("\n");
+
+            for (Page page: pages){
+                builder.append(page.getText());
+            }
+
+            builder.append("\n\n");
+            return builder.toString();
+        }
+    }
+
+
+    private static class Page{
+        @SerializedName("p")
+        Object [] content;
+
+        public String getText(){
+
+            StringBuilder builder = new StringBuilder();
+
+            for (Object text: content){
+                if (text instanceof String)
+                builder.append((String) text);
             }
 
             return builder.toString();
         }
     }
 
-    private static class Body{
-
-        @SerializedName("title")
-        private Title title;
-        @SerializedName("section")
-        private List<Section> section;
-
-//<body>
-//  <title>
-//   <p> Suzanne Collins</p>
-//        <p>The Hunger Games</p>
-//  </title>
-//  <section>
-//   <title>
-//        <p>Part I</p>
-//    <p>"The Tributes"</p>
-//   </title>
-//   <section>
-//    <title>
-//     <p>1</p>
-//    </title>
-    }
-
-
-    private static class Title{
-        @SerializedName("p")
-        Content content;
-    }
-    private static class Section{
-
-        @SerializedName("title")
-        private Title title;
-        @SerializedName("p")
-        private Content content;
-    }
-
-
-    private static class Content{
-
-        @SerializedName("p")
-        String text;
-    }
 
 }
